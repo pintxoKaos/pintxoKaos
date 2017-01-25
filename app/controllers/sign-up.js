3bdd53317300c22df2d38c90d4f9505912378bff
Fernando Javier Martínez-Conde Romero <fjavier.martinez-con.contractor@bbva.com>
2017-01-25 08:43:57 +0100

    adding favicon

--- app/controllers/sign-up.js
+++ app/controllers/sign-up.js
@@ -11,35 +11,36 @@ export default Ember.Controller.extend({
 	actions: {
 
 		createUser() {
-	    const auth = this.get('firebaseApp').auth();
-	    auth.createUserWithEmailAndPassword(
-				this.get('email'),
-				this.get('password')).
+				const auth = this.get('firebaseApp').auth();
+				auth.createUserWithEmailAndPassword(
+					this.get('email'),
+					this.get('password')).
 				then((userResponse) => {
 					console.log(userResponse);
-	      const user = this.store.createRecord('user', {
-	        uid: userResponse.uid,
-					alias: userResponse.email,
-					email: userResponse.email
-	      });
-	      return user.save();
-	    });
-	  },
-
-			signUp2() {
-				let controller = this;
-				this.get('firebaseApp').createUser({
-					email: this.get('email') || '',
-					password: this.get('password') || '',
-				}, (error, data) => {
-					if (error) {
-						console.log(error);
-					} else {
-						controller.set('email', null);
-						controller.set('password', null);
-						controller.transitionToRoute('sign-in');
-					}
+					const user = this.store.createRecord('user', {
+						uid: userResponse.uid,
+						alias: userResponse.email,
+						email: userResponse.email
+					});
+					return user.save();
 				});
-			}
+			},
+			/*
+						signUp2() {
+							let controller = this;
+							this.get('firebaseApp').createUser({
+								email: this.get('email') || '',
+								password: this.get('password') || '',
+							}, (error, data) => {
+								if (error) {
+									console.log(error);
+								} else {
+									controller.set('email', null);
+									controller.set('password', null);
+									controller.transitionToRoute('sign-in');
+								}
+							});
+						}
+			*/
 	}
 });
