42185e68f65e99c73a87669ca8155d8876d3e8df
Fernando Javier Martínez-Conde Romero <fjavier.martinez-con.contractor@bbva.com>
2017-01-27 09:35:32 +0100

    avoid jshint error

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,16 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+	errorMessage: null,
 	beforeModel() {
-			this.get('session').fetch().catch((error) => {
-				//console.log(error);
+		if (this.get('session')) {
+			this.get('session').fetch('password').catch((error) => {
+				this.set('errorMessage', error);
 			});
-		},
+		}
+	},
 
-		actions: {
-			accessDenied() {
-				this.transitionTo('/');
-			},
+	actions: {
+		accessDenied() {
+			this.transitionTo('/');
 		},
+	},
 
 });
