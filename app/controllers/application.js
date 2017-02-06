e88100d69b35270c92f7970d9b68de8a8d93ee4b
Fernando Javier Martínez-Conde Romero <fj.martinez-con.contractor@bbva.com>
2017-02-06 08:20:41 +0100

    rain fall

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,10 +1,11 @@
 import Ember from 'ember';
 
+
 export default Ember.Controller.extend({
 	languages: [{
 		name: 'ES',
 		code: 'es',
-		flagUrl: '../../assets/images/Spain.jpg'
+		flagUrl: '/assets/images/Spain.jpg'
 	}, {
 		name: 'UK',
 		code: 'en',
@@ -23,41 +24,31 @@ export default Ember.Controller.extend({
 		flagUrl: '/assets/images/Estelada_blava.svg.png'
 		/*flagUrl: 'http://www.sciencekids.co.nz/images/pictures/flags680/China.jpg'*/
 	}],
-/*
 
-<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-<!-- Include all compiled plugins (below), or include individual files as needed -->
-<!--script src="js/bootstrap.min.js"></script-->
-<script src="js/rainyday.js"></script>
-<script>
-function quellueva() {
-	var image = document.getElementById('background');
-	image.onload = function() {
-		var engine = new RainyDay({
-			image: this,
-			blur: 10,
-			opacity: 3,
-			gravityAngle: Math.PI / 2,
-			gravityAngleVariance: 0
-		});
+	ready: function() {
+		var image = document.getElementById('background');
+		image.onload = function() {
+			var engine = new RainyDay({
+				image: this,
+				blur: 10,
+				opacity: 3,
+				gravityAngle: Math.PI / 2,
+				gravityAngleVariance: 0
+			});
 
-		engine.gravity = engine.GRAVITY_NON_LINEAR;
-		engine.trail = engine.TRAIL_SMUDGE; //engine.trail = engine.TRAIL_DROPS
+			engine.gravity = engine.GRAVITY_NON_LINEAR;
+			engine.trail = engine.TRAIL_SMUDGE; //engine.trail = engine.TRAIL_DROPS
 
-		engine.rain([
-			[0, 3, 8],
-			[3, 8, 1]
-		], 50);
-	};
-	image.crossOrigin = 'anonymous';
-	image.src = 'images/bg4.jpg';
-
-}
-</script>
+			engine.rain([
+				[0, 3, 8],
+				[3, 8, 1]
+			], 50);
+		};
+		image.crossOrigin = 'anonymous';
+		image.src = '/assets/images/bg4.jpg';
+	},
 
 
-*/
 	selectedLanguage: Ember.computed('languages', 'locale',
 		function() {
 			return this.languages.findBy('code', this.get('locale'));
@@ -74,6 +65,29 @@ function quellueva() {
 	locale: Ember.computed.alias('i18n.locale'),
 
 	actions: {
+		quellueva(){
+			var image = document.getElementById('background');
+			//console.log(image.id);
+			image.onload = function() {
+				var engine = new RainyDay({
+					image: this,
+					blur: 10,
+					opacity: 3,
+					gravityAngle: Math.PI / 2,
+					gravityAngleVariance: 0
+				});
+
+				engine.gravity = engine.GRAVITY_NON_LINEAR;
+				engine.trail = engine.TRAIL_SMUDGE; //engine.trail = engine.TRAIL_DROPS
+
+				engine.rain([
+					[0, 3, 8],
+					[3, 8, 1]
+				], 50);
+			};
+			image.crossOrigin = 'anonymous';
+			image.src = '/assets/images/bg4.jpg';
+		},
 
 		changeLocale(param) {
 				this.get('i18n').set('locale', param);
